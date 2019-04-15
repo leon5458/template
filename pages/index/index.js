e9026f8dd2fa377d3ccc377d3bd8be24f6ac2eea
leon5458 <919633712@qq.com>
2019-04-15 14:00:11 +0800

    push

---
+++ pages/index/index.js
@@ -0,0 +1,93 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  click: function() {
+    console.log("---------点击所有的--------------")
+  },
+
+  click1: function() {
+    console.log("=======点击click1=======")
+  },
+  click2: function() {
+    console.log("=======点击click2=======")
+  },
+  click3: function() {
+    console.log("=======点击click3=======")
+  },
+  click_include1:function(){
+    console.log("===========点击了include1===========")
+  },
+
+  click_include2: function () {
+    console.log("===========点击了include2===========")
+  },
+
+  click_include3: function () {
+    console.log("===========点击了include3===========")
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
